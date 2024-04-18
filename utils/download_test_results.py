e31cde88d77831e50204c801a8f39f2c707986cd
Haojie Zhu <zhuhj@umich.edu>
2024-04-18 15:54:51 -0400

    update the data downloading code

--- utils/download_test_results.py
+++ utils/download_test_results.py
@@ -18,12 +18,16 @@ if __name__ == "__main__":
         "-r", "--round_number", help="Round number to download files from"
     )
     parser.add_argument("-t", "--team_id", help="Unique team ID")
+    parser.add_argument(
+        "-k", "--unique_key", help="Unique key for the file to download"
+    )
     args = parser.parse_args()
     round_number = args.round_number
     team_id = args.team_id
+    unique_key = args.unique_key
 
     local_download_path = "./test_data/round" + round_number
-    prefix = team_id + "/" + "round" + round_number
+    prefix = team_id + "_" + unique_key + "/" + "round" + round_number
 
     if not os.path.exists(local_download_path):
         os.makedirs(local_download_path)
@@ -34,7 +38,11 @@ if __name__ == "__main__":
         for obj in page.get("Contents", []):
             key = obj["Key"]
             filename = key.split("/")[-1]
-            download_path = os.path.join(local_download_path, filename)
+            download_path = key.replace(prefix, local_download_path)
+            download_folder = os.path.dirname(download_path)
+            print(download_folder, download_path)
+            if not os.path.exists(download_folder):
+                os.makedirs(download_folder, exist_ok=True)
 
             if not key.endswith("/"):
                 print(f"Downloading {key} to {download_path}")
